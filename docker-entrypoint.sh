0bcbd86b9c57bab0575b9dcf4d9f4a23ccbe17e3
Lucas Mösch <lucasmoesch@live.de>
2025-08-24 18:33:18 +0200

    Added webapp anvironment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# Generate web environment
+envsubst < /opt/phobos-lsx/apps/backend/public/js/env.template.js > /opt/phobos-lsx/apps/backend/public/js/env.js
+
+# Start the app
 cd "/opt/phobos-lsx/apps/backend" && npm run start
